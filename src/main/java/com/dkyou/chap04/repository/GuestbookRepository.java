59ad6ce9eecc382254c6c22408af4c9c7237e904
youdongkwan <dkyou7@naver.com>
2021-11-30 11:08:02 +0900

    feat: Querydsl 기본 사용 test

--- src/main/java/com/dkyou/chap04/repository/GuestbookRepository.java
+++ src/main/java/com/dkyou/chap04/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package com.dkyou.chap04.repository;
 
 import com.dkyou.chap04.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook,Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook,Long>, QuerydslPredicateExecutor<Guestbook> {
 }
