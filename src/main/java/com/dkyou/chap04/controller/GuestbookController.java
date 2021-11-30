45279605eed677401aa7057e94944847f3ab8eca
youdongkwan <dkyou7@naver.com>
2021-11-30 17:56:19 +0900

    feat: 등록 기능 구현

--- src/main/java/com/dkyou/chap04/controller/GuestbookController.java
+++ src/main/java/com/dkyou/chap04/controller/GuestbookController.java
@@ -1,5 +1,6 @@
 package com.dkyou.chap04.controller;
 
+import com.dkyou.chap04.dto.GuestbookDTO;
 import com.dkyou.chap04.dto.PageRequestDTO;
 import com.dkyou.chap04.dto.PageResultDTO;
 import com.dkyou.chap04.service.GuestbookService;
@@ -8,7 +9,9 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @Log4j2
@@ -29,5 +32,21 @@ public class GuestbookController {
 
         model.addAttribute("result",guestbookService.getList(pageRequestDTO));
         return "/guestbook/list";
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("register get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes){
+        log.info("dto..."+dto);
+
+        Long gno = guestbookService.register(dto);
+
+        redirectAttributes.addFlashAttribute("msg",gno);
+
+        return "redirect:/guestbook/list";
     }
 }
