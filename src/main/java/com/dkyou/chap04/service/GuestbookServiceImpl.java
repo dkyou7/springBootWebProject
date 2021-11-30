70690d8bd2e9e6e1286afee4da2cc03ce619c94f
youdongkwan <dkyou7@naver.com>
2021-11-30 16:08:53 +0900

    feat: Page DTO를 이용해서 처리하는 기능 추가

--- src/main/java/com/dkyou/chap04/service/GuestbookServiceImpl.java
+++ src/main/java/com/dkyou/chap04/service/GuestbookServiceImpl.java
@@ -1,11 +1,18 @@
 package com.dkyou.chap04.service;
 
 import com.dkyou.chap04.dto.GuestbookDTO;
+import com.dkyou.chap04.dto.PageRequestDTO;
+import com.dkyou.chap04.dto.PageResultDTO;
 import com.dkyou.chap04.entity.Guestbook;
 import com.dkyou.chap04.repository.GuestbookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+
+import java.util.function.Function;
 
 @Service
 @Log4j2
@@ -27,5 +34,16 @@ public class GuestbookServiceImpl implements GuestbookService{
 
         return entity.getGno();
 
+    }
+
+    @Override
+    public PageResultDTO<GuestbookDTO, Guestbook> getList(PageRequestDTO requestDTO) {
+        Pageable pageable = requestDTO.getPageable(Sort.by("gno").descending());
+
+        Page<Guestbook> result = repository.findAll(pageable);
+
+        Function<Guestbook,GuestbookDTO> fn = this::entityToDto;
+
+        return new PageResultDTO<>(result,fn);
     }
 }
--- src/test/java/com/dkyou/chap04/service/GuestbookServiceTest.java
+++ src/test/java/com/dkyou/chap04/service/GuestbookServiceTest.java
@@ -1,6 +1,9 @@
 package com.dkyou.chap04.service;
 
 import com.dkyou.chap04.dto.GuestbookDTO;
+import com.dkyou.chap04.dto.PageRequestDTO;
+import com.dkyou.chap04.dto.PageResultDTO;
+import com.dkyou.chap04.entity.Guestbook;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,5 +25,16 @@ class GuestbookServiceTest {
                 .build();
 
         System.out.println(service.register(guestbookDTO));
+    }
+
+    @Test
+    public void testList(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().page(1).size(10).build();
+
+        PageResultDTO<GuestbookDTO, Guestbook> resultDTO = service.getList(pageRequestDTO);
+
+        for(GuestbookDTO guestbookDTO : resultDTO.getDtoList()){
+            System.out.println(guestbookDTO);
+        }
     }
 }
