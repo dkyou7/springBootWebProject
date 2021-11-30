f2ff347e7e5a3361fd17713275bfb4a49315f37f
youdongkwan <dkyou7@naver.com>
2021-11-30 17:20:32 +0900

    feat: Page Web View 기능 구현

--- src/main/java/com/dkyou/chap04/controller/GuestbookController.java
+++ src/main/java/com/dkyou/chap04/controller/GuestbookController.java
@@ -1,19 +1,33 @@
 package com.dkyou.chap04.controller;
 
+import com.dkyou.chap04.dto.PageRequestDTO;
+import com.dkyou.chap04.dto.PageResultDTO;
+import com.dkyou.chap04.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Log4j2
 @RequestMapping("/guestbook")
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
-        log.info("list.........................");
+    private final GuestbookService guestbookService;
 
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public String list(PageRequestDTO pageRequestDTO, Model model){
+        log.info("list........................." + pageRequestDTO);
+
+        model.addAttribute("result",guestbookService.getList(pageRequestDTO));
         return "/guestbook/list";
     }
 }
