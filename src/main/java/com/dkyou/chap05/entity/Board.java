2eba58524321b27a041be3a0ceca00add9a481ad
dkyou <dkyou7@naver.com>
2021-12-01 21:31:53 +0900

    feat: Entity Init(엔티티간 관계 설정)

---
+++ src/main/java/com/dkyou/chap05/entity/Board.java
@@ -0,0 +1,24 @@
+package com.dkyou.chap05.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@ToString
+public class Board extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bno;
+
+    private String title;
+
+    private String content;
+
+    @ManyToOne
+    private Member writer;
+}
