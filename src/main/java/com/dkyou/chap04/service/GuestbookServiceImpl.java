4a7f54c1ffd0400992387c98a3796b5abcfae2ec
youdongkwan <dkyou7@naver.com>
2021-12-01 13:25:44 +0900

    feat: Guestbook read 기능 구현

--- src/main/java/com/dkyou/chap04/service/GuestbookServiceImpl.java
+++ src/main/java/com/dkyou/chap04/service/GuestbookServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -45,5 +46,12 @@ public class GuestbookServiceImpl implements GuestbookService{
         Function<Guestbook,GuestbookDTO> fn = this::entityToDto;
 
         return new PageResultDTO<>(result,fn);
+    }
+
+    @Override
+    public GuestbookDTO read(Long gno) {
+        Optional<Guestbook> result = repository.findById(gno);
+
+        return result.map(this::entityToDto).orElse(null);
     }
 }
