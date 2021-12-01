228718d0d681deaa46f4575e5f56931a703e6cca
dkyou <dkyou7@naver.com>
2021-12-01 22:06:30 +0900

    feat: 연관관계 다양한 테스트

--- src/test/java/com/dkyou/chap05/repository/BoardRepositoryTest.java
+++ src/test/java/com/dkyou/chap05/repository/BoardRepositoryTest.java
@@ -1,7 +1,48 @@
 package com.dkyou.chap05.repository;
 
+import com.dkyou.chap05.entity.Board;
+import com.dkyou.chap05.entity.Member;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class BoardRepositoryTest {
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Test
+    public void insertBoard(){
+        IntStream.rangeClosed(1,100).forEach(i ->{
+            Member member = Member.builder().email("user"+i+"@aaa.com")
+                    .build();
+
+            Board board = Board.builder()
+                    .title("Title..."+i)
+                    .content("Content..."+i)
+                    .writer(member)
+                    .build();
+
+            boardRepository.save(board);
+        });
+    }
+
+    @Test
+    @Transactional
+    public void testRead1(){
+        Optional<Board> result = boardRepository.findById(100L);
+
+        Board board = result.get();
+
+        System.out.println(board);
+        System.out.println(board.getWriter());
+    }
 
 }
--- src/test/java/com/dkyou/chap05/repository/ReplyRepositoryTest.java
+++ src/test/java/com/dkyou/chap05/repository/ReplyRepositoryTest.java
@@ -1,7 +1,47 @@
 package com.dkyou.chap05.repository;
 
+import com.dkyou.chap05.entity.Board;
+import com.dkyou.chap05.entity.Reply;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+import java.util.stream.IntStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class ReplyRepositoryTest {
 
+    @Autowired
+    private ReplyRepository replyRepository;
+
+    @Test
+    public void insertReply(){
+
+        IntStream.rangeClosed(1,300).forEach(i ->{
+            long bno = (long)(Math.random() * 100) + 1;
+
+            Board board = Board.builder().bno(bno).build();
+
+            Reply reply = Reply.builder()
+                    .text("Reply........"+i)
+                    .board(board)
+                    .replyer("guest")
+                    .build();
+
+            replyRepository.save(reply);
+        });
+    }
+
+    @Test
+    public void testReply1(){
+        Optional<Reply> result = replyRepository.findById(98L);
+
+        Reply reply = result.get();
+
+        System.out.println(reply);
+        System.out.println(reply.getBoard());
+    }
 }
