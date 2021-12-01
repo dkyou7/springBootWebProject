17da7e3e1b7c07aa48f148238dfa1ca9825b9dc8
dkyou <dkyou7@naver.com>
2021-12-01 21:35:08 +0900

    feat: Repository Init

---
+++ src/main/java/com/dkyou/chap05/repository/ReplyRepository.java
@@ -0,0 +1,7 @@
+package com.dkyou.chap05.repository;
+
+import com.dkyou.chap05.entity.Reply;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ReplyRepository extends JpaRepository<Reply,Long> {
+}
